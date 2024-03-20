5ad336875769ccaec775427b317bc1357725bdc4
Ariel A <arzamendiaariel@gmail.com>
2024-03-20 16:30:39 -0300

    prevent name collision

--- src/index.ts
+++ src/index.ts
@@ -13,26 +13,56 @@ export default class extends Controller {
     }
   }
 
-  add (e: Event) {
+  add(e: Event) {
     e.preventDefault()
 
-    const content: string = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime().toString())
+    // Generate a unique identifier based on the current time
+    const uniqueId = new Date().getTime().toString()
+
+    // Clone the template content and replace NEW_RECORD with uniqueId
+    let content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, uniqueId)
+
+    // Insert the cloned and modified content before the target
     this.targetTarget.insertAdjacentHTML('beforebegin', content)
+
+    // Find the last set of fields added and adjust their attributes
+    const lastAddedFields = this.element.querySelector(`[data-fields-id="${uniqueId}"]`);
+    if (lastAddedFields) {
+      lastAddedFields.querySelectorAll('input, select, textarea').forEach((input) => {
+        if (input.hasAttribute('name')) {
+          const updatedName = input.getAttribute('name').replace(/\[\d+\]/, `[${uniqueId}]`);
+          input.setAttribute('name', updatedName);
+        }
+        if (input.hasAttribute('id')) {
+          const updatedId = input.getAttribute('id').replace(/_\d+/, `_${uniqueId}`);
+          input.setAttribute('id', updatedId);
+        }
+      });
+
+      // Update the 'for' attributes of associated labels
+      lastAddedFields.querySelectorAll('label').forEach((label) => {
+        if (label.hasAttribute('for')) {
+          const updatedFor = label.getAttribute('for').replace(/_\d+/, `_${uniqueId}`);
+          label.setAttribute('for', updatedFor);
+        }
+      });
+    }
   }
 
-  remove (e: Event): void {
+  remove(e: Event): void {
     e.preventDefault()
 
-    // @ts-ignore
-    const wrapper: HTMLElement = e.target.closest(this.wrapperSelectorValue)
+    //@ts-ignore
+    // Find the closest wrapper using the provided selector value
+    const wrapper = e.target.closest(this.wrapperSelectorValue)
 
     if (wrapper.dataset.newRecord === 'true') {
-      wrapper.remove()
+      wrapper.remove() // Remove the wrapper if it's a newly added record
     } else {
-      wrapper.style.display = 'none'
-
-      const input: HTMLInputElement = wrapper.querySelector("input[name*='_destroy']")
-      input.value = '1'
+      wrapper.style.display = 'none' // Hide the wrapper for existing records
+      //@ts-ignore
+      const input = wrapper.querySelector("input[name*='_destroy']")
+      input.value = '1' // Mark the record for destruction on form submission
     }
   }
 }
